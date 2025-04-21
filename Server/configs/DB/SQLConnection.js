01bc0a1fb7386972d5f483b7fab626842cd9723a
Yerick Aguilar <97064039+yerick1235@users.noreply.github.com>
2025-04-21 17:46:09 -0600

    Update SQLConnection.js
    Cambios en credenciales de Coneccion a DB

--- Server/configs/DB/SQLConnection.js
+++ Server/configs/DB/SQLConnection.js
@@ -3,11 +3,11 @@ import { createConnection } from "mysql2"
 export const connection = () => {
     try {
         const pool = createConnection({
-            host:'spjam.h.filess.io',
+            host:'qypmh.h.filess.io',
             port:'3307',
-            user:'PruebaCam_notedboxbe',
-            password:'7866fbdc97febab3f74708610cbd877c34d21e59',
-            database:'PruebaCam_notedboxbe'
+            user:'DBCamGuard_factletter',
+            password:'f54c4ac80188988a194c85e8d3de70025c0bd55b',
+            database:'DBCamGuard_factletter'
         })
         console.log('DB Connection Successful !!')
         return pool
